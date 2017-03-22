c86fccf304f94fbdf1bed186c15a09ed34f0c80a
Coen van der Kamp <coen@fourdigits.nl>
2017-03-22 21:57:37 +0100

    Put the Wagtail app in the wagtailrobot dir

--- wagtailrobot/wsgi.py
+++ wagtailrobot/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for robotnao project.
+WSGI config for wagtailrobot project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,6 +13,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "robotnao.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wagtailrobot.settings.dev")
 
 application = get_wsgi_application()
