2870beacf83ec986471d164a5d5f94db363bf29f
Lars van de Kerkhof <lars@permanentmarkers.nl>
2017-03-22 13:52:45 +0100

    haha

--- wagtailrobot/behaviour/robotinterface.py
+++ wagtailrobot/behaviour/robotinterface.py
@@ -4,13 +4,16 @@ from django.utils.functional import cached_property
 
 class NaoConnection(object):
     def __init__(self):
-        self.app = qi.Application(url="tcp://koe.local:9559")
+        self.app = qi.Application(url="tcp://172.16.0.107:9559")
         self.app.start()
         self.session = self.app.session
 
     @cached_property
     def voice(self):
-        return self.session.service("ALTextToSpeech")
+        voice = self.session.service("ALTextToSpeech")#pitchShift
+        voice.setParameter("pitchShift", 1.0)
+        voice.setParameter("speed", 0.5)
+        return voice
 
     def say(self, message, **kwargs):
         return self.voice.say(message, **kwargs)
