894ec3ef049bbed647e030f39d4b87c00bbf9489
Mike <mike.overkamp@gmail.com>
2017-03-22 15:22:45 +0100

    use decorators to connect to signals

--- home/signals.py
+++ home/signals.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.signals import user_logged_in, user_logged_out
+from django.dispatch import receiver
 
 from wagtailrobot.behaviour.robotinterface import NaoConnection
 conn = NaoConnection()
 
 
+@receiver(user_logged_in)
 def say_hello(sender, user, request, **kwargs):
     name = user.get_full_name()
     if not name:
@@ -11,12 +13,9 @@ def say_hello(sender, user, request, **kwargs):
     conn.voice.say("Hello {}".format(name))
 
 
+@receiver(user_logged_out)
 def say_goodby(sender, user, request, **kwargs):
     name = user.get_full_name()
     if not name:
         name = user.username
     conn.voice.say("Goodbye {}".format(name))
-
-
-user_logged_in.connect(say_hello)
-user_logged_out.connect(say_goodby)
