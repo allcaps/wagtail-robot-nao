f3f58741407c51a12941f178aa2e0ca48b97c1be
Coen van der Kamp <coen@fourdigits.nl>
2017-03-23 13:36:01 +0100

    Remove talk. Add final party.

--- wagtailrobot/home/middleware.py
+++ wagtailrobot/home/middleware.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 
+import time
 from django.utils.deprecation import MiddlewareMixin
 from django.utils.translation import ugettext as _
 
@@ -14,5 +15,11 @@ class PageViewMiddleware(MiddlewareMixin):
         except KeyError:
             return response
 
-        conn.voice.say(_("Do you like your page, {}?").format(page.title))
+        conn.voice.say(_("Do you like your page?"))
+        conn.voice.say(_("I think it is awesome! LETS PARTY!"))
+        conn.voice.say(_("Dear {}... May I have this dance?".format(request.user.get_full_name())))
+        conn.play('User/gangnamstyle-5c4934/gangnam1')
+        conn.voice.say(_("That was fun. Thank you very much! Hope to see you soon!"))
+
         return response
+
