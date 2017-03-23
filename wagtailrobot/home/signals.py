a128667d1bcf84fccd4e951bfbcbf20c484a8dc1
Mike <mike.overkamp@gmail.com>
2017-03-23 13:51:27 +0100

    async stuff

--- wagtailrobot/home/signals.py
+++ wagtailrobot/home/signals.py
@@ -1,3 +1,4 @@
+from thread import start_new_thread
 from django.conf import settings
 from django.contrib.auth.signals import (
     user_logged_in,
@@ -22,14 +23,12 @@ else:
 
 @receiver(user_logged_in)
 def say_hello(sender, user, request, **kwargs):
+    conn.findFaces()
     name = user.get_full_name()
     if not name:
         name = user.username
-    conn.findFaces()
     conn.voice.say(_("Hello {name}").format(name=name))
-    conn.playAsync("animations/Sit/Emotions/Neutral/AskForAttention_1")
-    conn.asyncTakePicturePNG(name)
-    conn.voice.say(_("Create a page: Add a title, image and click \\emph=2\\ 'Save draft'."))
+    start_new_thread(conn.voice.say, (_("Create a page: Add a title, image and click \\emph=2\\ 'Save draft'."),))
 
 
 @receiver(user_logged_out)
@@ -53,11 +52,14 @@ def give_comment_on_page_title(sender, instance, revision, **kwargs):
     adjective = _("cool")
     if _("page") in title or _("title") in title:
         adjective = _("stupid")
-        conn.playAsync('animations/Stand/Emotions/Negative/Angry_1')
+        start_new_thread(conn.playAsync, ("animations/Stand/Emotions/Negative/Angry_1",))
     conn.voice.say(_("{title} is a {adjective} title for a page").format(title=title, adjective=adjective))
     conn.voice.say(_("Your page is now published. It is the top item in the list. View it by clicking \\pau=1\\ \\emph=3\\ live. The live button is in the status column."))
 
 
 @receiver(user_login_failed)
 def give_password_hint(*args, **kwargs):
+    conn.findFaces()
+    name = kwargs['credentials']['username']
+    start_new_thread(conn.asyncTakePicturePNG, (name,))
     conn.voice.say(_("1? 2? 3? Is really your password? That can't be secure! Try: 1. 2. 3. \\pau=2\\ \\emph=2\\ 4!"))
